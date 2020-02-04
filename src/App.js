64ddb5a1286f4d14ce5d6102ef4e2386dbd21073
mikkopuustinen <mikkowr@gmail.com>
2020-02-04 17:25:56 +0200

    starting on the interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+
+const View = () => {
+  return (
+    <div className='View'>
+      <p>meeting</p>
+    </div>
+  )
+
+}
+
+const Sidebar = () => {
+
+  return (
+    <div className='Sidebar'>
+      <p>side</p>
+    </div>
+  )
+}
+
+
 function App() {
+  const [meetings, setMeetings] = useState([])
+
+  useEffect(() => {
+    fetch('./build-front/data/meetings.json')
+      .then(r => r.json())
+      .then(function(d) {
+        setMeetings(d)
+      } )
+  })
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <View></View>
+      <Sidebar></Sidebar>
+      
     </div>
   );
 }
